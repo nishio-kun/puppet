bd2d8a9655793fcac9ad85c79441f6020b3a3181
nishio-kun <soreil310lune@gmail.com>
2020-03-10 16:44:59 +0900

    Add setup file

---
+++ setup.sh
@@ -0,0 +1,10 @@
+apt install ffmpeg
+mkdir ~/data
+
+# DensePose
+git clone https://github.com/nishio-kun/DensePose.git
+mkdir ./DensePose/input
+mkdir ./DensePose/output
+
+# vid2vid
+git clone https://github.com/nishio-kun/vid2vid.git
